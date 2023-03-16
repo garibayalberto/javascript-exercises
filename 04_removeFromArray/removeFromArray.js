55d38b0867c244aab97741a6e74b23aee53312ae
Alberto <garibayalberto98@gmail.com>
2023-03-16 12:06:45 -0700

    Completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array , ...remove) {
 
+  //let removeArray = Array.from(remove);
+
+  for(let i = 0 ; i < remove.length; i++) {
+    const index = array.indexOf(remove[i]);
+    if (index > -1) { 
+      array.splice(index, 1);
+    }
+  }
+  
+  return array;
 };
 
 // Do not edit below this line
