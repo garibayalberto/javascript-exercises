44b4609ba777457750d079b2091fc22e4adbd291
Alberto <garibayalberto98@gmail.com>
2023-03-16 11:15:07 -0700

    Completed Exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+  
 
+  if(times < 0) {
+    return "ERROR";
+  }
+  else{
+    let phrase = "";
+
+    for(let i = 0; i < times; i++ ){
+      phrase += word;
+    }
+
+    return phrase;
+  }
+  
 };
 
 // Do not edit below this line
