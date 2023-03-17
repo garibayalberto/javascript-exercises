719a69c0a14162df2187b09e44cae320dfbd371e
Alberto <garibayalberto98@gmail.com>
2023-03-17 12:14:14 -0700

    Completed Exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,24 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+  if( Number.isInteger(year / 4) ){
+
+    if( Number.isInteger(year / 100) ) {
+
+      if (Number.isInteger(year / 400)) {
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+    
+    return true;
+  }
+  else{
+    return false;
+  }
+  
+  
 };
 
 // Do not edit below this line
