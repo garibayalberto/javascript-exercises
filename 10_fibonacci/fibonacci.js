d293b256d6418901c0c445afbff95c7385e96f3e
Alberto <garibayalberto98@gmail.com>
2023-03-31 23:41:50 -0700

    Completed Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  
+  //My answer
+  if( num < 0 ) {
+    return "OOPS";
+  }
+
+  if (num == 1) {
+    return 1;
+  }
+  else if(num == 0){
+    return 0;
+  }
+  else{
+    return fibonacci(num-1) + fibonacci(num-2);
+  }
 
 };
 
