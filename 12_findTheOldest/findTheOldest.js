92b43d498c673e940199b33df81fa92e2a00c676
Alberto <garibayalberto98@gmail.com>
2023-04-05 14:10:33 -0700

    Completed Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+  return people.reduce( (oldest, person) => {
+    const date = new Date();
+
+    let oldestAge = (oldest.yearOfDeath ? oldest.yearOfDeath : date.getFullYear()) - oldest.yearOfBirth;
+    let personAge = (person.yearOfDeath ? person.yearOfDeath : date.getFullYear()) - person.yearOfBirth;
+
+    if(oldestAge < personAge){
+      oldest = person;
+    }
+    
+    return oldest;
+  });
 
 };
 
