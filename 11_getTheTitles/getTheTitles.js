7a02cf27357e46e32247e8a4443ae1e56486d564
Alberto <garibayalberto98@gmail.com>
2023-04-04 21:16:38 -0700

    Completed exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+  return books.reduce( (books, book) => {
+    books.push(book.title);
+    return books;
+  }, [] );
 };
 
+//Vanilla JS array functions 
 // Do not edit below this line
 module.exports = getTheTitles;
